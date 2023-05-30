700aaa07a7ec6a28c524abecec95c786d5e8605b
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-05-30 21:38:57 +0700

    Initial Commit

---
+++ ADACatur/ADACaturApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADACaturApp.swift
+//  ADACatur
+//
+//  Created by Ivan on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ADACaturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
