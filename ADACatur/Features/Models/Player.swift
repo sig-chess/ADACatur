1d58ba20e5b1b3c115d4a71bd185ab689984219e
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-06-10 19:10:58 +0700

    feat: init Player and PlayerRow

---
+++ ADACatur/Features/Models/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  ADACatur
+//
+//  Created by Ivan on 10/06/23.
+//
+
+import Foundation
+
+struct Player {
+    var id: UUID = UUID()
+    let name: String
+    let email: String
+    let eloScore: Double
+}
